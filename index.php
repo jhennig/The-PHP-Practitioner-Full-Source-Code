f8fd6e43512079183e97708fc7d88a9be5ec0a10
jhennig <jeremy.hennig@ualberta.ca>
2021-03-03 15:19:55 -0700

    Update index.php
    added some error catching

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 
-require 'vendor/autoload.php';
-require 'core/bootstrap.php';
+ini_set('display_errors',1);
+
+session_start();
+
+require '../../framework/vendor/autoload.php';
+require '../../framework/core/bootstrap.php';
+
 
 use App\Core\{Router, Request};
+try{
 
-Router::load('app/routes.php')
+Router::load('../../framework/app/routes.php')
     ->direct(Request::uri(), Request::method());
+	
+	
+} catch(Exception $e) {
+ require('../../framework/app/views/partials/head.php');
+ echo $e->getMessage();
+ 
+}
